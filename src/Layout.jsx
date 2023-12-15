f38d1d38cce2adc2e896156d58d05e472f1c32c5
demjhonsilver <djsilverph@gmail.com>
2023-12-15 11:13:50 +0800

    Add page contents

---
+++ src/Layout.jsx
@@ -0,0 +1,33 @@
+
+import PropTypes from 'prop-types';
+
+
+
+
+import  { Header } from './layouts/Header'; 
+import { Footer } from './layouts/Footer';
+
+
+
+function Layout({ children }) {
+
+
+
+
+  return (
+    <div className="app">
+      { <Header />  } {/* Conditionally render HeaderBlog or Header */}
+
+      <main className="content">
+        {children}
+      </main>
+      { < Footer />}
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Layout;
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,26 @@
+
+
+export const Footer = () => {
+  return (
+    <div>
+        
+        <footer className="bg-dark text-light py-4">
+      <div className="container">
+   
+
+        <div className="row">
+          <div className="col-md-12 text-center">
+            &copy; {new Date().getFullYear()} Demjhons
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+
+
+    </div>
+  )
+}
